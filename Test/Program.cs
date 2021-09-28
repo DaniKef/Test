8a585b3e08920dc1552ebac8f184b3fde329ab2d
DaniilH <gurenko888@gmail.com>
2021-09-28 22:26:08 +0300

    Добавьте файлы проекта.

---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Daniil";
+            var age = 19;
+            var isHuman = true;
+
+            Type nameType = name.GetType();
+            Type ageType = age.GetType();
+            Type isHumanType = isHuman.GetType();
+
+            Console.WriteLine("Name type is " + nameType.ToString());
+            Console.WriteLine("Age type is " + ageType.ToString());
+            Console.WriteLine("IsHuman type is " + isHumanType.ToString());
+            return;
+        }
+    }
+}
